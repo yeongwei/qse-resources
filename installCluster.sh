7aa87eb4f619c91fe6d499a300e3146be70da559
YeongWei <laiyw@persistent.my>
2017-02-08 02:36:22 -0500

    Make hostname as replaceable variable CLUSTER_HOST_NAME

--- installCluster.sh
+++ installCluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CLUSTER_NAME=npi
-export MGMT1=v-npiprod01
+export MGMT1=#CLUSTER_HOST_NAME#
 source functions
 registerBlueprint $MGMT1 qse-1-nodes-42 blueprint.json
 installCluster
